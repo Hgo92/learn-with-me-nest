ceb2343a2ebe702b54b3e3116b3eaf7c4183c3b1
Hgo92 <hugo_monier@yahoo.fr>
2026-05-15 14:04:56 +0200

    start - lancement du projet

---
+++ typeorm.config.ts
@@ -0,0 +1,12 @@
+// typeorm.config.ts
+import { DataSource } from 'typeorm';
+import { config } from 'dotenv';
+
+config();
+
+export default new DataSource({
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
+  entities: ['src/**/*.entity.ts'],
+  migrations: ['src/migrations/*.ts'],
+});
